dfbc30ab3370859f2441de3d6f722bb20aad9b46
Ridge <rto@MBAir.local>
2017-08-02 12:00:45 -0700

    hook up initial pan handler

--- Components/CardContainer.js
+++ Components/CardContainer.js
@@ -21,10 +21,14 @@ export default class SwipeCards extends Component {
       onMoveShouldSetResponderCapture: () => true,
       onMoveShouldSetPanResponderCapture: () => true,
 
+      // Set initial values to 0 (center of the screen)
       onPanResponderGrant: (e, gestureState) => {
+        this.state.pan.setValue({x: 0, y: 0});
       },
 
+      // Set Delta values to the states pan position when object is dragged/panned
       onPanResponderMove: Animated.event([
+        null, {dx: this.state.pan.x, dy: this.state.pan.y},
       ]),
 
       onPanResponderRelease: (e, {vx, vy}) => {
@@ -33,8 +37,18 @@ export default class SwipeCards extends Component {
   }
 
   render() {
+
+    // Destructure the value of pan from the state
+    let { pan } = this.state;
+
+    // Calculate the x and y transform from the pan value
+    let [translateX, translateY] = [pan.x, pan.y];
+
+    // Calculate the transform property and set it as a value for style
+    let cardStyle = {transform: [{translateX}, {translateY}]};
+
     return (
-      <Animated.View {...this._panResponder.panHandlers}>
+      <Animated.View style={cardStyle} {...this._panResponder.panHandlers}>
         <View style={styles.card}/>
       </Animated.View>
     );
