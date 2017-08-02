8e88f4792525b1366be140075ead64c5e6a9fb37
Ridge <rto@MBAir.local>
2017-08-02 12:01:41 -0700

    fix typo in state constructor

--- Components/CardContainer.js
+++ Components/CardContainer.js
@@ -11,7 +11,7 @@ export default class SwipeCards extends Component {
 
   constructor(props) {
     super(props);
-    this.state {
+    this.state = {
       pan: new Animated.ValueXY()
     };
   }
